0865fa371c1a6d044e800d2ef3f63e42e9c3b6c3
Leila <nb.leila10@gmail.com>
2019-02-26 14:47:33 -0600

    Setup app and add navbar

---
+++ src/styles/generalStyles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  margin: auto;
+`
+
+export const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
